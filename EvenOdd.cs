7190b517c87e416d119bc7b46af44249a9fd9c46
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-01-05 16:30:00 +0530

    Add files via upload

---
+++ EvenOdd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class EvenOdd
+    {
+        public static void EvenOdd1() 
+        {
+            int i;
+            Console.Write("Enter a Number : ");
+            i = int.Parse(Console.ReadLine());
+
+            if (i % 2 == 0)
+            {
+                Console.Write("Entered Number is an Even Number");
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("Entered Number is an Odd Number");
+                Console.Read();
+            }
+
+        }
+    }
+}
---
+++ LargestNum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class LargestNum
+    {
+        public static void FindTheLargestNum()
+        {
+            int num1, num2, num3;
+
+            Console.WriteLine("Enter The 1st Number");
+            num1=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter The 2st Number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter The 3st Number");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine("1st number is greatest among three");
+                }
+                else
+                {
+                    Console.WriteLine("3st number is greatest among three");
+                }
+            }
+            else if (num2 > num3)
+            {
+                Console.WriteLine("2st number is greatest among three");
+            }
+            else 
+            {
+                Console.WriteLine("3st number is greatest among three");
+            }
+        }
+    }
+}
